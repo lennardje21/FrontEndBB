0dc17b78c63679ac78ecfced1e03a4cfc272ed58
Lennard Touw <lennard.touw@gmail.com>
2022-04-04 11:31:48 +0200

    One Page site voor CLE3. Wie zit er in het team, wat is het concept en het prototype.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* deze functie zorgt ervoor dat de navigatie in mobile versie kan in en uit klappen */
+
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+
+    if (visibility === "false"){
+        primaryNav.setAttribute('data-visible', true)
+        navToggle.setAttribute('aria-expanded', true)
+    } else {
+        primaryNav.setAttribute('data-visible', false)
+        navToggle.setAttribute('aria-expanded', false)
+    }
+})
